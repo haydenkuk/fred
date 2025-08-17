8b127dbfa10e959247222b944047e53c5663b12e
Yunjong Guk <haydenkuk@gmail.com>
2025-08-17 01:44:04 -0400

    0.0.0 version init

---
+++ fred/base/fred.py
@@ -0,0 +1,44 @@
+import requests
+import os
+import datetime as dt
+
+
+from dotenv import load_dotenv, find_dotenv
+from ..params import vars
+from ..params.params import (
+  Apikey,
+  FileType,
+)
+from ..params.exceptions import MissingApikey
+from typing import (
+  Dict, Any
+)
+DOTENV = load_dotenv(find_dotenv())
+
+
+class Fred:
+
+  @classmethod
+  def request(
+    cls,
+    endpoint: str,
+    params: Dict[str, Any] = {},
+  ) -> Dict:
+    if not vars.APIKEY:
+      if DOTENV:
+        apikey = os.getenv('FRED_APIKEY')
+
+        if apikey:
+          vars.APIKEY = Apikey(apikey)
+    
+    if not vars.APIKEY:
+      raise MissingApikey()
+
+    url = '/'.join([vars.BASEURL, endpoint])
+    params['api_key'] = str(vars.APIKEY)
+    params['file_type'] = str(FileType())
+    response = requests.get(
+      url,
+      params=params)
+    data = response.json()
+    return data
